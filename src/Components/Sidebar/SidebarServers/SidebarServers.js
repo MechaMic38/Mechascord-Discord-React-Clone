a06af2310f698d91ca1b295fb1f850d2a75394cc
MechaMic38 <69167202+MechaMic38@users.noreply.github.com>
2020-10-15 18:37:46 +0200

    Added Avatar, linking to my personal website

--- src/Components/Sidebar/SidebarServers/SidebarServers.js
+++ src/Components/Sidebar/SidebarServers/SidebarServers.js
@@ -12,6 +12,7 @@ import { setCSModalView, setFSModalView } from "../../../Redux/appSlice";
 import { motion } from "framer-motion";
 import { selectUser } from "../../../Redux/userSlice";
 import { firestore } from "firebase";
+import { Avatar } from "@material-ui/core";
 
 function SidebarServers() {
   const dispatch = useDispatch();
@@ -86,6 +87,18 @@ function SidebarServers() {
       >
         <ExploreIcon />
       </div>
+
+      <a
+        href="https://mechamic38.netlify.app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="sidebarServers__visitMe"
+      >
+        <Avatar
+          src="https://avatars1.githubusercontent.com/u/69167202?s=460&u=f92c409b9b3020714d34c3aa56d3697b10d08204&v=4"
+          alt="Pluda Michael (a.k.a. MechaMic_38)"
+        />
+      </a>
     </div>
   );
 }
